6c557fbd383fba5ad4884f06ac81bf6015bc03a8
tanmaieraghavasri-web <tanmaieraghavasri@gmail.com>
2026-04-17 17:42:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react(), tailwindcss()],
+    define: {
+      'process.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
